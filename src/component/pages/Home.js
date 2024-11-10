94c88c3afe659462e2c6b2b4fd3a44dbaed104a7
CryptoCoderr <55534242+CryptoCoderr@users.noreply.github.com>
2024-11-10 13:11:55 +0530

    update home ui shift footer at bottom

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -63,104 +63,105 @@ function Home() {
   return (
     <div>
       {/* Header */}
-      <div className="bg-gray-500 text-white py-10">
+      <div className="bg-gray-500 text-white py-10 	">
         <div className="containerkws">
           <div className="flex justify-between">
             <div className="font-bold">GoHighLevel</div>
           </div>
         </div>
       </div>
-
-      <div className="bgimage h-80">
-        <div className="bg-[rgba(0,0,0,0.3)] h-full ">
-          <div className="containerkws h-full">
-            <div className="text-white flex items-center justify-center h-full text-5xl font-bold ">
-              GoHighLevel Weather app
+      <div className="min-h-[80vh]">
+        <div className="bgimage h-80 ">
+          <div className="bg-[rgba(0,0,0,0.3)] h-full ">
+            <div className="containerkws h-full">
+              <div className="text-white flex items-center justify-center h-full text-5xl font-bold ">
+                GoHighLevel Weather app
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Search box */}
-      <div className="bg-gray-300 py-4">
-        <div className="containerkws">
-          <div className="relative flex">
-            <div className="flex rounded overflow-hidden w-full">
-              <input
-                type="text"
-                className="bg-white w-80 p-2 focus:outline-none"
-                placeholder="e.g. london"
-                value={search}
-                onChange={(e) => setsearch(e.target.value)}
-              />
-              <div
-                className="bg-black p-2 text-white cursor-pointer"
-                onClick={handleSearchSubmit}
-              >
-                Search
-              </div>
-            </div>
-
-            {/* Search results */}
-            {serach_data.length > 0 && (
-              <div className="absolute w-80 bg-white shadow-lg rounded mt-5 z-10">
-                {serach_data.map((res, index) => (
-                  <div
-                    key={index}
-                    onClick={() => {
-                      fetchWeatherData("", res.id);
-                      setserach_data([]);
-                    }}
-                  >
-                    <div className="p-3 flex justify-between items-center cursor-pointer">
-                      <div>
-                        {res.name}, {res.sys.country}
-                      </div>
-                      <div>{Math.round(res.main.temp)}°C</div>
-                    </div>
-                    {index !== serach_data.length - 1 && <hr />}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-
-      {/* Weather details */}
-      {weatherDetails && (
-        <div className="my-5 ">
+        {/* Search box */}
+        <div className="bg-gray-300 py-4">
           <div className="containerkws">
-            <div className="text-red-500">
-              {new Date(weatherDetails.dt * 1000).toLocaleString()}
-            </div>
-            <div className="font-bold text-2xl flex gap-2">
-              {weatherDetails.name}, {weatherDetails.sys.country}
-            </div>
-            <div className="text-4xl flex gap-2 mt-3 items-center">
-              <FaTemperatureHigh className="text-2xl" />
-              {Math.round(weatherDetails.main.temp)}°C
-            </div>
-            <div>
-              Feels like {Math.round(weatherDetails.main.feels_like)}°C.{" "}
-              {weatherDetails.weather[0].description}.
-            </div>
-            <div className="flex gap-3 mt-2">
-              <div>
-                <div>Wind: {Math.round(weatherDetails.wind.speed)} m/s</div>
-                <div>Humidity: {weatherDetails.main.humidity}%</div>
-                <div>
-                  Dew point: {Math.round(weatherDetails.main.temp_min)}°C
+            <div className="relative flex">
+              <div className="flex rounded overflow-hidden w-full">
+                <input
+                  type="text"
+                  className="bg-white w-80 p-2 focus:outline-none"
+                  placeholder="e.g. london"
+                  value={search}
+                  onChange={(e) => setsearch(e.target.value)}
+                />
+                <div
+                  className="bg-black p-2 text-white cursor-pointer"
+                  onClick={handleSearchSubmit}
+                >
+                  Search
                 </div>
               </div>
-              <div>
-                <div>Pressure: {weatherDetails.main.pressure} hPa</div>
-                <div>Visibility: {weatherDetails.visibility / 1000} km</div>
-              </div>
+
+              {/* Search results */}
+              {serach_data.length > 0 && (
+                <div className="absolute w-80 bg-white shadow-lg rounded mt-5 z-10">
+                  {serach_data.map((res, index) => (
+                    <div
+                      key={index}
+                      onClick={() => {
+                        fetchWeatherData("", res.id);
+                        setserach_data([]);
+                      }}
+                    >
+                      <div className="p-3 flex justify-between items-center cursor-pointer">
+                        <div>
+                          {res.name}, {res.sys.country}
+                        </div>
+                        <div>{Math.round(res.main.temp)}°C</div>
+                      </div>
+                      {index !== serach_data.length - 1 && <hr />}
+                    </div>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
         </div>
-      )}
+
+        {/* Weather details */}
+        {weatherDetails && (
+          <div className="my-5 ">
+            <div className="containerkws">
+              <div className="text-red-500">
+                {new Date(weatherDetails.dt * 1000).toLocaleString()}
+              </div>
+              <div className="font-bold text-2xl flex gap-2">
+                {weatherDetails.name}, {weatherDetails.sys.country}
+              </div>
+              <div className="text-4xl flex gap-2 mt-3 items-center">
+                <FaTemperatureHigh className="text-2xl" />
+                {Math.round(weatherDetails.main.temp)}°C
+              </div>
+              <div>
+                Feels like {Math.round(weatherDetails.main.feels_like)}°C.{" "}
+                {weatherDetails.weather[0].description}.
+              </div>
+              <div className="flex gap-3 mt-2">
+                <div>
+                  <div>Wind: {Math.round(weatherDetails.wind.speed)} m/s</div>
+                  <div>Humidity: {weatherDetails.main.humidity}%</div>
+                  <div>
+                    Dew point: {Math.round(weatherDetails.main.temp_min)}°C
+                  </div>
+                </div>
+                <div>
+                  <div>Pressure: {weatherDetails.main.pressure} hPa</div>
+                  <div>Visibility: {weatherDetails.visibility / 1000} km</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
